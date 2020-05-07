9a382ca2571eb922accf8d0a0703b32d3e1cf092
Ilya <ilust98@gmail.com>
2020-05-07 12:02:47 +0300

    refs replaced

--- src/app/chat/Chat.js
+++ src/app/chat/Chat.js
@@ -6,33 +6,39 @@ class Chat extends React.Component
     constructor(props) {
         super(props);
         this.submit = this.submit.bind(this);
-        this.state = { messages : JSON.parse(localStorage.messages || "[]") };
+        this.state = {
+            messages : JSON.parse(localStorage.messages || "[]"),
+            currentMessageText : "",
+        };
         onstorage = () => {
             this.setState({ messages : JSON.parse(localStorage.messages || "[]") }) 
         }
     }
 
     submit() {
-        let {_text} = this.refs;
-        if (_text.value === "") {
+        if (this.state.currentMessageText === "") {
             return;
         }
         const newMessage = {    
             author : this.props.user,
             datetime : new Date().toGMTString(),
-            text : _text.value,
+            text : this.state.currentMessageText,
         };
-        _text.value = "";
         const newMessages = this.state.messages.concat(newMessage);
-        this.setState({ messages : newMessages });
         localStorage.messages = JSON.stringify(newMessages);
+        this.setState({
+            messages : newMessages,
+            currentMessageText : "",
+        });
     }
 
     render() {
         return (
             <div id="chat">
                 <button id="logout" onClick={this.props.logout}> Log out </button>
-                <textarea ref="_text"></textarea>
+                <textarea value={this.state.currentMessageText}
+                          onChange={ev => this.setState({currentMessageText : ev.target.value})}>
+                </textarea>
                 <button type="submit" onClick={this.submit}> Send </button>
                 <div id="messages">
                     {this.state.messages.map((message, i) => <Message key={i} {...message} />)}
--- src/app/login/Login.js
+++ src/app/login/Login.js
@@ -6,25 +6,41 @@ class Login extends React.Component
     constructor(props) {
         super(props);
         this.submit = this.submit.bind(this);
-        this.state = { error_message : "" };
+        this.state = {
+            login : "",
+            password : "",
+            entryError : "",
+        };
     }
  
     submit(ev) {
         ev.preventDefault(); 
-        const {_login, _password} = this.refs;
-        if (passwords[_login.value] === _password.value) {
-            this.props.onSuccess(_login.value);
+        const {login, password} = this.state;
+        if (passwords[login] === password) {
+            this.props.onSuccess(login);
         } else {
-            this.setState({ error_message : "Wrong login or password" });
+            this.setState({ entryError : "Wrong login or password" });
         }
     }
 
     render() {
         return (
             <form onSubmit={this.submit}>
-                <label> Login <input type="text" ref="_login" /> </label> <br/>
-                <label> Password <input type="password" ref="_password" /> </label> <br/>
-                <p> {this.state.error_message} </p> <br/>
+                <label> Login
+                    <input type="text"
+                           value={this.state.login}
+                           onChange={ ev => this.setState({ login : ev.target.value }) }
+                    />
+                </label> <br/>
+
+                <label> Password
+                    <input type="password"
+                           value={this.state.password}
+                           onChange={ ev => this.setState({ password : ev.target.value }) }
+                    />
+                </label> <br/>
+
+                <p> {this.state.entryError} </p> <br/>
                 <button type="submit"> Enter </button>
             </form>
         );
